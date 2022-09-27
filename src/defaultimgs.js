e9076fc7e3c1debc4c9af50e048558f7c9b2bbc0
nithin88-ops <58203293+nithin88-ops@users.noreply.github.com>
2022-09-28 01:23:24 +0530

    final frontend commit2

--- src/defaultimgs.js
+++ src/defaultimgs.js
@@ -1,4 +1,4 @@
 export const defaultImgs = [
-  "https://ipfs.moralis.io:2053/ipfs/QmNgA9MNWFfRaoKzBt21VghQopnKXBgVxzyGvv5qjsV4Vw/media/1",
+  "https://avatars.dicebear.com/api/adventurer/Nithin%20Shanmugam.svg",
   "https://ipfs.moralis.io:2053/ipfs/QmNgA9MNWFfRaoKzBt21VghQopnKXBgVxzyGvv5qjsV4Vw/media/2",
 ];
